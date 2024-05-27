6429e87cb5b4ae6d756a80d062f3b9f1f4079bb1
Benjamin Rush <bnjmnrsh@gmail.com>
2024-05-27 16:27:29 +0100

    Extract type defs into their own file.

---
+++ typedefs.mjs
@@ -0,0 +1,20 @@
+/**
+ * An EXIF tag preference, which may be:
+ *      - A string, the EXIF key name. This will simply include the tag value in the output, if it has been populated on the media file.
+ *      - An object with the key and value. In this case, if the tag is not populated on the media file, the fallback value will be used if the key is not populated on the media file.
+ *      - The value may also optionally be a key, with a val and write boolean. The write boolean is optional and defaults to false.
+ *
+ * @typedef {Object} ExifTag
+ * @property {String | { [key: String]: { val: String, write?: Boolean } | null } } key - A string of EXIF key name, or an object with the key and value. The value may also optionally be a key, with a val and write boolean. The write boolean is optional and defaults to true.
+ */
+
+/**
+ * User Options object
+ *
+ * @typedef {Object} Options - The default options.
+ * @property {String} srcDir - Relative path of directory to traverse.
+ * @property {String} outputPath - The relative path of the directory to write output.
+ * @property {String} fileName - The name of the output file with extension.
+ * @property {Array.<ExifTag>} exifTags - An array of EXIF tags to extract.
+ * @property {Array.<string>} validExtensions - An array of valid media extensions.
+ */
