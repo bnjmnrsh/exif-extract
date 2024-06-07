4efd4d59a7bffb14f71f218a5bb8901e25037980
Benjamin Rush <bnjmnrsh@gmail.com>
2024-06-07 13:46:55 +0100

    Subject: Bump exiftool-vendored to 26.2.0 to enable flatened tags in output. Reason for Commit: Prior to 26.2.0, exiftool-vendored passed the `-struct` flag which forced structured output without falttened tags. Raised in issue #184, the `struct` porperty has been added when creating new instances. However, its 'undefined' value which should align to ExifTools default output is currently broken. I believe that this would be the optimal behavior, however 0 or 2 would suffice. We will want to be able to pass this to the user in a later refactor.
    Summary:
    Flatend tags are now able to be included in output - Yay!
    
    Notes:
    https://github.com/photostructure/exiftool-vendored.js/issues/184
    
    https://github.com/photostructure/exiftool-vendored.js/commit/add424c92a9b45606f69aaefe724db23d1612e41

--- modules/exiftool-instance.mjs
+++ modules/exiftool-instance.mjs
@@ -1,9 +1,12 @@
 import { ExifTool } from 'exiftool-vendored'
 
+// Passing desired flags for to the ExifTool instance isn't working atm
+// See: https://github.com/photostructure/exiftool-vendored.js/issues/184
+
 // https://exiftool.org/struct.html#:~:text=By%20default%2C%20tags%20are%20copied,is%20disabled%3B%20see%20below.)
 // https://exiftool.org/ExifTool.html#Struct
 // https://exiftool.org/struct.html
 
 export const exifExtract = new ExifTool({
-  exiftoolArgs: ['-stay_open', 'True', '-@', '-', '-taco'] //, '-api', 'struct=2'
+  struct: 0 // undefined | 0 | 1 | 2
 })
